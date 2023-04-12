eab84e0552f47ed3ddf2cc646d05f031a6d453c1
rustiandacode <rustiandazen09@gmail.com>
2023-04-12 16:02:16 +0700

    added toogle menu on mobile

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 const menu = document.querySelector('.menu')
+const subMenu1 = document.querySelector('.sub-menu-1')
+const subMenu2 = document.querySelector('.sub-menu-2')
+const subMenu3 = document.querySelector('.sub-menu-3')
+const subMenu4 = document.querySelector('.sub-menu-4')
 const ul = document.querySelector('ul')
 
-menu.addEventListener('click', () => {
+const toogleMenu = () => {
   ul.classList.toggle('show')
-})
+}
+
+menu.addEventListener('click', () => toogleMenu())
+subMenu1.addEventListener('click', () => toogleMenu())
+subMenu2.addEventListener('click', () => toogleMenu())
+subMenu3.addEventListener('click', () => toogleMenu())
+subMenu4.addEventListener('click', () => toogleMenu())
